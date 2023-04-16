f1915b9c6613493f62864dfb9b9e0b781b53e195
davnish <davnish@Nischals-MacBook-Air.local>
2023-04-16 11:25:35 +0530

    Added speechtotext

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('specific', views.specific, name='specific'),
-    path('getResponse', views.getResponse, name='getResponse')
+    path('getResponse', views.getResponse, name='getResponse'),
+    path('speechtotext', views.speechtotext, name = 'speechtotext'),
+    # path('speaktext', views.SpeakText, name = 'speaktext')
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import openai, os
 from dotenv import load_dotenv
-
+import speech_recognition as sr
+import pyttsx3
+import json
 load_dotenv()
 
 api_key = os.getenv("OPENAI_KEY", None)
@@ -10,9 +12,6 @@ print(api_key)
 
 def index(request):
     return render(request, 'blog/index.html')
-
-def specific(request):
-    return HttpResponse("killer")
 
 def getResponse(request):
     chatResponse = None
@@ -24,10 +23,39 @@ def getResponse(request):
         chatResponse = openai.Completion.create(
             engine='text-davinci-003',
             prompt=prompt,
-            max_tokens = 256,
+            max_tokens = 100,
             temperature =0.3
         )
+
     return HttpResponse(chatResponse["choices"][0]["text"])
 
+# def SpeakText(request):
+     
+#     # Initialize the engine
+#     engine = pyttsx3.init()
+#     engine.say(request.POST.get('Message'))
+#     engine.runAndWait()
 
+def speechtotext(request):
+		
+	r = sr.Recognizer()
+	Query = {
+		"success": "False",
+		"text": None
+	}
+	with sr.Microphone() as source:
+
+		print('Listening')
+		r.pause_threshold = 0.7
+		audio = r.listen(source,phrase_time_limit=6)
+	try:
+		print("Recognizing")
+		Query["text"] = r.recognize_google(audio)
+		Query["success"] = "True"
+	except Exception:
+		# Query = False
+		Query["text"] = "Sorry I didnt understand, can you please repeat that again."
+
+	print(Query["text"])
+	return HttpResponse(json.dumps(Query))
 
